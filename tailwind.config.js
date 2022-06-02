4ccb4fed8c21ffb669681f5bb17ba7d1b600a4ae
cphan70 <vanchiphan70@gmail.com>
2022-06-02 10:27:23 +0700

    commit all code

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      height:{
+        custom:"620px"
+      },
+      spacing: {
+        "40-c":"40rem"
+      }
+    },
+  },
+  plugins: [],
+}
